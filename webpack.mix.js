d175d7b581ca66486773591e44e959e80e485723
Andrey Koens <brokenfans@gmail.com>
2017-10-15 16:27:10 -0200

    [dependency] adicionado npm e webpack com laravel-mix

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+
+    //IMPORTS
+        let mix = require('laravel-mix');
+        let ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+
+    //CONFIGURATION
+        mix.webpackConfig( {
+            plugins: [
+                new ImageminPlugin( {
+                    // disable: process.env.NODE_ENV !== 'production', // Disable during development
+                    pngquant: {
+                        quality: '95-100',
+                    },
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                } ),
+            ],
+        } );
+
+    //BUILD
+        mix.js('src/js/app.js', 'dist/assets/js')
+           .sass('src/scss/app.scss', 'dist/assets/css')
+           .copy( 'src/img', 'dist/assets/img', false );
